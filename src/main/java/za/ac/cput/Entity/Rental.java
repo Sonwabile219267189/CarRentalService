01d06453f46e0dc5c3085c261b31dd5d8725af10
Sonwabile <219267189@mycput.ac.za>
2022-03-14 23:53:06 +0200

    Added Rental class in Entity Package and added target file to .gitignore file

--- src/main/java/za/ac/cput/Entity/Rental.java
+++ src/main/java/za/ac/cput/Entity/Rental.java
@@ -1,4 +1,47 @@
+/**Rental.java
+ * Entity for the Rental
+ * Author: Sonwabile Gxoyiya (219267189)
+ * Date: 24 March 2022
+ */
+
 package za.ac.cput.Entity;
 
 public class Rental {
+
+    private boolean isRented;
+
+    public Boolean getIsRented(){
+        return isRented;
+    }
+
+    private Rental(){}
+
+    private Rental(Builder builder){
+        this.isRented = builder.isRented;
+    }
+
+    public static class Builder{
+        private boolean isRented;
+
+        public Builder setIsRented(boolean isRented){
+            this.isRented = isRented;
+            return this;
+        }
+
+        public Builder copy(Rental rental){
+            this.isRented = rental.isRented;
+            return this;
+        }
+
+        public Rental build(){
+            return new Rental(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Rental{" +
+                "isRented=" + isRented +
+                '}';
+    }
 }
--- src/test/java/za/ac/cput/Entity/RentalTest.java
+++ src/test/java/za/ac/cput/Entity/RentalTest.java
@@ -1,7 +1,19 @@
 package za.ac.cput.Entity;
 
+import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RentalTest {
+
+    @Test
+    public void testRental(){
+        Rental rental = new Rental.Builder()
+                .setIsRented(true)
+                .build();
+        assertNotNull(rental);
+        System.out.println(rental.toString());
+    }
 
 }
