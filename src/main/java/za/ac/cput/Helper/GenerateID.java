4d406f09255b07baa1a94d9780e11403867496ac
mbuyi33 <alphmbuyi33@gmail.com>
2022-04-09 21:12:23 +0200

    First commit , entity class and Factory with Factory classes. Payment and Schedule classes.

---
+++ src/main/java/za/ac/cput/Helper/GenerateID.java
@@ -0,0 +1,17 @@
+package za.ac.cput.Helper;
+/** GenerateID.java
+        * Class to generate ID. To be used in other classes.
+        * Author: Hlombekazi Mbelu (209024666)
+        * Date: 8 April 2022
+        */
+import java.util.UUID;
+
+public class GenerateID {
+
+        public static String generateUUID(){
+            UUID uuid = UUID.randomUUID();
+            return uuid.toString();
+        }
+
+
+}
